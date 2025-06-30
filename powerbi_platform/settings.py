0b770f9773f909bf29e81a871c424287f10f73e2
Steven Rodriguez Pineda <estiven94ops@gmail.com>
2025-06-30 17:36:06 -0500

    Tercer commit

--- powerbi_platform/settings.py
+++ powerbi_platform/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dirdo(xcdsy0r)2u@m*-a^!-az-uf$h#70kz)gh3%#m*zcrok='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['auditcaats.pythonanywhere.com']
 
 # Application definition
 
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
