2cea30d34002410c2cf78972c074baded58a2f04
Steven Rodriguez Pineda <estiven94ops@gmail.com>
2025-06-30 17:49:35 -0500

    Quinto commit - cambios en settings.py

--- powerbi_platform/settings.py
+++ powerbi_platform/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dirdo(xcdsy0r)2u@m*-a^!-az-uf$h#70kz)gh3%#m*zcrok='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
