7b61015bca090aa508f1bff0f523bb00d40edbd2
Steven Rodriguez Pineda <estiven94ops@gmail.com>
2025-06-30 16:23:59 -0500

    Primer commit

---
+++ dashboard/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = None
+
+        try:
+            user_obj = User.objects.get(email=email)
+            user = authenticate(request, username=user_obj.username, password=password)
+        except User.DoesNotExist:
+            pass
+
+        if user:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'error': 'Correo o contrasena inválidas'})
+    return render(request, 'login.html')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
